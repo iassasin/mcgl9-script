6dc9b31ec015c758e7a23ce1c1cbe33d3376db7c
iassasin <iassasin@yandex.ru>
2020-11-06 23:59:00 +0300

    add config for bundle 3d lib

---
+++ mcgl9/3d-export.js
@@ -0,0 +1,15 @@
+import * as td from './3d';
+
+Mesh = td.Mesh;
+Scene = td.Scene;
+Transform = td.Transform;
+TransformMatrix = td.TransformMatrix;
+Voxel = td.Voxel;
+createCustomTransform = td.createCustomTransform;
+createMeshFromTemplate = td.createMeshFromTemplate;
+degToRad = td.degToRad;
+vec3 = td.vec3;
+vecAddVec = td.vecAddVec;
+vecInvert = td.vecInvert;
+vecMultVal = td.vecMultVal;
+vecNormalize = td.vecNormalize;
--- rollup.config.js
+++ rollup.config.js
@@ -4,39 +4,72 @@ import { uglify } from "rollup-plugin-uglify";
 
 const mini = process.env.MINI === '1';
 const watch = process.env.WATCH === '1';
+const target = process.env.TARGET || 'script';
+const allTargets = target === 'all';
 
-let plugins = [
-  resolve(),
-  typescript(),
-];
+function optional(cond, obj) {
+  if (cond) {
+    return [obj];
+  }
 
-if (mini) {
-  plugins.push(uglify({
-    compress: {
-      passes: 3,
-    },
-
-    output: {
-      max_line_len: 128,
-    },
-  }));
+  return [];
 }
 
-export default {
-  input: 'script.ts',
-  context: 'this',
+function createConfig() {
+  let plugins = [
+    resolve(),
+    typescript(),
+  ];
 
-  output: {
-    file: 'dist/script.js',
-    format: 'iife',
-    compact: false,
-    strict: false,
-  },
+  if (mini) {
+    plugins.push(uglify({
+      compress: {
+        passes: 3,
+      },
 
-  watch: {
-    exclude: 'node_modules/**',
-    clearScreen: false,
-  },
+      output: {
+        max_line_len: 128,
+      },
+    }));
+  }
 
-  plugins,
-};
+  return {
+    input: 'script.ts',
+    context: 'this',
+
+    watch: {
+      exclude: 'node_modules/**',
+      clearScreen: false,
+    },
+
+    plugins,
+  };
+}
+
+export default [
+  ...optional(allTargets || target === 'script', {
+    ...createConfig(),
+
+    input: 'script.ts',
+
+    output: {
+      file: 'dist/script.js',
+      format: 'iife',
+      compact: false,
+      strict: false,
+    },
+  }),
+
+  ...optional(allTargets || target === '3d' , {
+    ...createConfig(),
+
+    input: 'mcgl9/3d-export.js',
+
+    output: {
+      file: 'dist/3d.js',
+      format: 'iife',
+      compact: false,
+      strict: false,
+    },
+  }),
+];
